7d5cfcdecb5fb08ed2171eccad64bbd1756eae1c
Mohsen Malekifard <mohsen.malekifard.dev@gmail.com>
2025-08-08 10:48:22 +0000

    B

---
+++ my-app/src/data/products.js
@@ -0,0 +1,5 @@
+// src/data/products.js
+export const products = [
+  { id: 1, name: 'کفش ورزشی', price: 300000, image: 'https://via.placeholder.com/150' },
+  { id: 2, name: 'کتونی نایک', price: 450000, image: 'https://via.placeholder.com/150' },
+];
